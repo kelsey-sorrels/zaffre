0f8ecad645428ebcb70ea180bc4b9f40d63389e4
Aaron Santos <aaronds109@gmail.com>
2016-02-14 17:18:47 -0800

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,12 @@
 (defproject zaffre "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :description "A fast Clojure console library"
+  :url "https://github.com/aaron-santos/zaffre"
+  :license {:name "The MIT License (MIT)"
+            :url "https://raw.githubusercontent.com/aaron-santos/zaffre/master/LICENSE"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/core.async "0.2.374"]
+                 [hello_lwjgl/lwjgl "2.9.1"]
+                 [im.bci/pngdecoder "0.13"]
+                 [com.taoensso/timbre "4.2.1"]
+                 [clojure-watch "0.1.11"]]
+  :main zaffre.glterminal)
