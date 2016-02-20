fe1c9cf2f3cd081a815083db3dc87d13fc52c287
Aaron Santos <aaronds109@gmail.com>
2016-02-20 13:00:09 -0800

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
                  [im.bci/pngdecoder "0.13"]
                  [com.taoensso/timbre "4.2.1"]
                  [clojure-watch "0.1.11"]]
-  :lein-release {:deploy-via :clojars
+  :lein-release {:deploy-via :lein-deploy
                  :scm :git
                  :build-uberjar true}
   :main zaffre.glterminal)
