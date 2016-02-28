fd948dfbd85b6d490d326caa2ffa90237566a90e
Aaron Santos <aaronds109@gmail.com>
2016-02-28 10:32:30 -0800

    Reduce retro.fs fx amount

--- resources/retro.fs
+++ resources/retro.fs
@@ -7,8 +7,8 @@ out vec3 outcolor;
 
 vec4 sample(vec2 co) {
     vec4 sum = vec4(0.0);
-    vec2 hstep = vec2(0.0015, 0.0);
-    vec2 vstep = vec2(0.0, 0.0015);
+    vec2 hstep = vec2(0.0012, 0.0);
+    vec2 vstep = vec2(0.0, 0.0012);
 
     sum += texture(uFb, vTextureCoord + co);
     sum += texture(uFb, vTextureCoord + co + hstep) * 0.125;
@@ -20,7 +20,7 @@ vec4 sample(vec2 co) {
     sum += texture(uFb, vTextureCoord + co - hstep + vstep) * 0.0625;
     sum += texture(uFb, vTextureCoord + co - vstep - vstep) * 0.0625;
 
-    return sum / 1.2;
+    return sum / 1.3;
 }
     
 void main(void){
@@ -31,7 +31,7 @@ void main(void){
     float green = sample(vec2( 0.0000, 0.0)).y;
     float blue  = sample(vec2(-0.0003, 0.0)).z;
     
-    outcolor.r = mix(0.5 * red, red, wavePos);
-    outcolor.g = mix(0.5 * green, green, wavePos);
-    outcolor.b = mix(0.5 * blue, blue, wavePos);
+    outcolor.r = mix(0.75 * red, red, wavePos);
+    outcolor.g = mix(0.75 * green, green, wavePos);
+    outcolor.b = mix(0.75 * blue, blue, wavePos);
 }
