5d4e4acca5a9f559bc98f0d017d1c600d8297516
Aaron Santos <aaronds109@gmail.com>
2018-06-30 22:11:03 -0700

    Use real http client to follow redirects

--- project.clj
+++ project.clj
@@ -5,13 +5,13 @@
             :url "https://raw.githubusercontent.com/aaron-santos/zaffre/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/core.async "0.3.465"]
-                 ;[aaron-santos/lwjgl "3.0.0rc1"]
                  ^{:voom {:repo "https://github.com/rogerallen/hello_lwjgl" :branch "master"}}
-                 [hello_lwjgl "0.4.0"]
+                 [hello_lwjgl "0.4.0-20180105_044833-g683b3f6"]
                  [commons-io/commons-io "2.5"]
                  [overtone/at-at "1.2.0"]
                  [aaron-santos/tinter "0.1.1-SNAPSHOT"]
                  [nio "1.0.3"]
+                 [clj-http "3.9.0"]
                  [org.joml/joml "1.7.1"]
                  [im.bci/pngdecoder "0.13"]
                  [com.taoensso/timbre "4.2.1"]
@@ -24,5 +24,5 @@
                     (.contains "mac"))
               ["-XstartOnFirstThread"
                "-Dorg.lwjgl.opengl.Display.enableHighDPI=true"
-               "-Djava.library.path=native/"]
-              ["-Djava.library.path=native/"]))
+               #_"-Djava.library.path=native/"]
+              [#_"-Djava.library.path=native/"]))
