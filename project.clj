ed4ba1bd40b840d7e229825a8e5bdf6cc42803a2
Kelsey Sorrels <kelsey.d.sorrels@gmail.com>
2023-12-04 20:35:20 -0800

    Add nio dependency

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.2.374"]
                  [hello_lwjgl/lwjgl "2.9.1"]
+                 [nio "1.0.3"]
                  [im.bci/pngdecoder "0.13"]
                  [com.taoensso/timbre "4.2.1"]
                  [clojure-watch "0.1.11"]]
