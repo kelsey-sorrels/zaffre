38e8a277797ec1a3a01b543baeba0bbe3a44474e
Kelsey Sorrels <kelsey.d.sorrels@gmail.com>
2023-12-04 20:35:21 -0800

    Fix CP437 font sheet rendering

--- src/zaffre/imageutil.clj
+++ src/zaffre/imageutil.clj
@@ -85,18 +85,25 @@
     (log/info "scaled-bytes" scaled-bytes)
     (->Image (* s width) (* s height) channels scaled-bytes)))
 
+
+(defn copy-buffer-range! [dest src n]
+  (doseq [_ (range n)]
+    (.put dest (.get src))))
+
 (defn copy-sub-image [{dwidth :width dheight :height dchannels :channels dbytes :byte-buffer :as dimg}
                       {swidth :width sheight :height schannels :channels sbytes :byte-buffer}
                       dx1 dy1 sx1 sy1 sx2 sy2]
   {:pre [(= dchannels schannels)]}
-  (doseq [x (range (- sx2 sx1))
-          y (range (- sy2 sy1))
+  ;; for each line
+  (doseq [y (range (- sy2 sy1))
           :let [sidx (* (+ sx1 (* (+ y sy1) swidth)) schannels)
-                didx (* (+ dx1 (* (+ x dy1) dwidth)) dchannels)]]
+                didx (* (+ dx1 (* (+ y dy1) dwidth)) dchannels)]]
       (.position dbytes didx)
       (.position sbytes sidx)
-      (doseq [_ (range dchannels)]
-        (.put dbytes (.get sbytes))))
+      (copy-buffer-range! dbytes sbytes (* (- sx2 sx1) schannels)))
+  (.position sbytes (* swidth sheight schannels))
+  (.position dbytes (* dwidth dheight dchannels))
+  (.flip sbytes)
   (.flip dbytes)
   dimg)
 
@@ -105,18 +112,6 @@
                   x y]
   (copy-sub-image dest-img src-img x y 0 0 width height))
                       
-
-(defn- copy-channel [v channel]
-  (let [channel-shift (case channel
-                       :alpha 24
-                       :blue  16
-                       :green  8
-                       :red    0)
-        c (->
-            (bit-shift-left 0xFF channel-shift)
-            (bit-and v)
-            (unsigned-bit-shift-right channel-shift))]
-    (reduce (fn [a n] (bit-or a (bit-shift-left c n))) 0 [24 16 8 0])))
 
 (defn resize [{swidth :width sheight :height :as img} width height]
   (copy-sub-image (image width height)
@@ -160,6 +155,7 @@
             g (.get byte-buffer)
             b (.get byte-buffer)]
         (.put dest-buffer (byte (quot (+ r g b) 3)))))
+    (.flip byte-buffer)
     (.flip dest-buffer)
     (->Image width height 1 dest-buffer)))
 
@@ -171,6 +167,7 @@
         (.put dest-buffer v)
         (.put dest-buffer v)
         (.put dest-buffer v)))
+    (.flip byte-buffer)
     (.flip dest-buffer)
     (->Image width height 1 dest-buffer)))
 
@@ -183,12 +180,16 @@
         (.put dest-buffer v)
         (.put dest-buffer v)
         (.put dest-buffer v)))
+    (.flip byte-buffer)
     (.flip dest-buffer)
     (->Image width height 1 dest-buffer)))
 
 (defmethod mode :rgb->rgba
   [{:keys [width height channels byte-buffer]} image-type]
+  (log/info "rgb->rgba")
   (let [dest-buffer (BufferUtils/createByteBuffer (* width height (num-channels image-type)))]
+    (log/info "dest-buffer" dest-buffer)
+    (log/info "pixels" (quot (.limit byte-buffer) channels))
     (doseq [i (range (quot (.limit byte-buffer) channels))]
       (let [r (.get byte-buffer)
             g (.get byte-buffer)
@@ -197,6 +198,7 @@
         (.put dest-buffer g)
         (.put dest-buffer b)
         (.put dest-buffer (byte 0))))
+    (.flip byte-buffer)
     (.flip dest-buffer)
     (->Image width height 4 dest-buffer)))
 
@@ -211,18 +213,41 @@
         (.put dest-buffer r)
         (.put dest-buffer g)
         (.put dest-buffer b)))
+    (.flip byte-buffer)
     (.flip dest-buffer)
     (->Image width height 3 dest-buffer)))
+
+(defn- skip [buffer n]
+  (.position buffer (+ (.position buffer) n)))
+
+(defn- copy-channel [v channel]
+  (let [channel-shift (case channel
+                       :alpha 24
+                       :blue  16
+                       :green  8
+                       :red    0)
+        c (->
+            (bit-shift-left 0xFF channel-shift)
+            (bit-and v)
+            (unsigned-bit-shift-right channel-shift))]
+    (reduce (fn [a n] (bit-or a (bit-shift-left c n))) 0 [24 16 8 0])))
 
 (defn copy-channels [{:keys [width height channels byte-buffer]} channel]
   (let [img           (image width height)
         result-buffer (BufferUtils/createByteBuffer (* width height channels))]
     (doseq [x (range width)
             y (range height)]
-      (.putLong result-buffer
-                (copy-channel
-                  (.getLong byte-buffer)
-                  channel)))
+      (let [c (case channel
+                :red   0
+                :green 1
+                :blue  2
+                :alpha 3)]
+      (skip byte-buffer c)
+      (let [v (.get byte-buffer)]
+        (dotimes [_ channels]
+          (.put result-buffer v)))
+      (skip byte-buffer (dec (- channels c)))))
+    (.flip byte-buffer)
     (.flip result-buffer)
     (log/info "copy-channels" result-buffer)
     (->Image width height channels result-buffer)))
